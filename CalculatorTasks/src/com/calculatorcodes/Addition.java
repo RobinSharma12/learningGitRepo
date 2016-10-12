6d823ee08b9f355500730c3cf57fd45611eafbce
RobinSharma12 <robin.424242@gmail.com>
2016-10-13 03:05:00 +0530

    These are the second commit - modified add() method in Addition.java and newly added Multiplication.java

--- CalculatorTasks/src/com/calculatorcodes/Addition.java
+++ CalculatorTasks/src/com/calculatorcodes/Addition.java
@@ -2,10 +2,10 @@ package com.calculatorcodes;
 
 public class Addition {
 	public static int add(int a, int b) {
-		return a*b;
+		return a+b;
 	}
 	public static void main(String[] args) {
-		System.out.println(Addition.add(10,20));
+		System.out.println("The result is "+Addition.add(10,20));
 
 	}
 
---
+++ CalculatorTasks/src/com/calculatorcodes/Multiplication.java
@@ -0,0 +1,12 @@
+package com.calculatorcodes;
+
+public class Multiplication {
+	public static int mul(int a, int b) {
+		return a*b;
+	}
+	public static void main(String[] args) {
+		System.out.println("The result is "+Multiplication.mul(10,20));
+
+	}
+
+}
