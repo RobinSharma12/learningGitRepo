137ebb2a29f29beb606938b4f14a8ec1c91f6ed0
RobinSharma12 <robin.424242@gmail.com>
2016-10-13 02:37:08 +0530

    First Addition class commit for add() method.
    Signed-off-by: RobinSharma12 <robin.424242@gmail.com>

---
+++ CalculatorTasks/src/com/calculatorcodes/Addition.java
@@ -0,0 +1,12 @@
+package com.calculatorcodes;
+
+public class Addition {
+	public static int add(int a, int b) {
+		return a*b;
+	}
+	public static void main(String[] args) {
+		System.out.println(Addition.add(10,20));
+
+	}
+
+}
